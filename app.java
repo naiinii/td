35d52695d1a9dcb6ead09217ab200b23341d58cd
noela doh <dohnoela59@gmail.com>
2022-02-23 20:18:51 +0000

    modified app.java

--- app.java
+++ app.java
@@ -2,3 +2,4 @@ Welcome to TDBank
 Your finantial partner
 Open an account with td and get $300
 Bug removed from the application
+review the app
--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
 Testing the application
+for deployment
