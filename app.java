22e10eb1e7f96e1c9ae10075079e121c685405e5
femi140 <99775061+femi140@users.noreply.github.com>
2022-02-23 15:46:40 -0500

    Update app.java

--- app.java
+++ app.java
@@ -3,3 +3,6 @@ Your finantial partner
 Open an account with td and get $300
 Bug removed from the application
 review the app
+#2 is corrected  'financial' partner
+Td is on the top five list in Canada.
+  Good Job!
